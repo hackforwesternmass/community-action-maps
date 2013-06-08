5cad9fba2337b75b931d9e5fedefef6ab00333a5
jrabbit <jackjrabbit+github@gmail.com>
2013-06-08 16:30:09 -0400

    more refactor http://pinkie.mylittlefacewhen.com/media/f/img/mlfw9615-Untitled.png

--- parse_xlsxto3tractcsv.py
+++ parse_xlsxto3tractcsv.py
@@ -4,37 +4,36 @@ import xml.etree.ElementTree as ET
 import xlrd
 import sys
 
-fipslist=[]
-#skip header row, go through rest of data
+def getfips(headerdata,):
+	fipslist=[]
+	#skip header row, go through rest of data
 
-#This block of code attaches FIPS to addresses 
-for rownum in range(sh.nrows)[1:sh.nrows]:
-	address=sh.row_values(rownum)[addressfield]+","
-# Hard coding in Massachusetts!
-    	city=sh.row_values(rownum)[cityfield]+", Ma"
-    	zip=sh.row_values(rownum)[zipfield]
-    	buildurl='http://rpc.geocoder.us/service/csv?address='+address+'+'+city+'+'+zip
-# get rid of ridiculous unicode nonbreaking spaces and all spaces
-	buildurl=buildurl.replace(u'\xa0', u'').replace(' ','+')
-# switch type to string
-	burlstr=buildurl.encode('ascii','ignore')
-	out=urlgrabber.urlread(burlstr)
+	#This block of code attaches FIPS to addresses 
+	for rownum in range(sh.nrows)[1:sh.nrows]:
+		address=sh.row_values(rownum)[addressfield]+","
+	# Hard coding in Massachusetts!
+	    	city=sh.row_values(rownum)[cityfield]+", Ma"
+	    	zipcode=sh.row_values(rownum)[zipfield]
+	    	buildurl='http://rpc.geocoder.us/service/csv?address='+address+'+'+city+'+'+zipcode
+	# get rid of ridiculous unicode nonbreaking spaces and all spaces
+		buildurl=buildurl.replace(u'\xa0', u'').replace(' ','+')
+	# switch type to string
+		burlstr=buildurl.encode('ascii','ignore')
+		outp=urlgrabber.urlread(burlstr)
 
-# If address not resolved, skip it, assign 999999 tract code:
-	if out != "2: couldn't find this address! sorry":
-		lat=out.split(",")[0]
-		lon=out.split(",")[1]
-		buildcensurl='http://data.fcc.gov/api/block/2010/find?latitude='+lat+'&longitude='+lon
-		outblock=urlgrabber.urlread(buildcensurl)
-		e = ET.fromstring(outblock)
-		block = e.find('{http://data.fcc.gov/api}Block')
-		fipstract=block.attrib['FIPS'][0:11]
-	else:
-		fipstract='99999999999'
-	fipslist.append(fipstract)
-
-
-
+	# If address not resolved, skip it, assign 999999 tract code:
+		if outp != "2: couldn't find this address! sorry":
+			lat=outp.split(",")[0]
+			lon=outp.split(",")[1]
+			buildcensurl='http://data.fcc.gov/api/block/2010/find?latitude='+lat+'&longitude='+lon
+			outblock=urlgrabber.urlread(buildcensurl)
+			e = ET.fromstring(outblock)
+			block = e.find('{http://data.fcc.gov/api}Block')
+			fipstract=block.attrib['FIPS'][0:11]
+		else:
+			fipstract='99999999999'
+		fipslist.append(fipstract)
+	return fipslist
 
 #This block of code does aggregation
 countarray={}
@@ -85,6 +84,7 @@ def main(inp, out):
 		elif vars.value == 'income':
 			incomefield=col
 		col=col+1
+	fips = getfips(sh)
 
 
 if __name__ == '__main__':
