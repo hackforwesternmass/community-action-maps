57284133b0177ef69bf6ff84067bd6c2cc209152
jrabbit <jackjrabbit+github@gmail.com>
2013-06-26 21:01:50 -0400

    control tabs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(function(){
 	$('a.programs-tab').click(function() {
 		$('#programs').toggle();
+		$('#demos').hide();
+		$('#resources').hide();
+	});
+	$('a.demos-tab').click(function(){
+		$('#demos').toggle();
+		$('#programs').hide();
+		$('#resources').hide();
+	});
+	$('a.resources-tab').click(function() {
+		$('#resources').toggle();
+		$('#demos').hide();
+		$('#programs').hide();
 	});
 });
